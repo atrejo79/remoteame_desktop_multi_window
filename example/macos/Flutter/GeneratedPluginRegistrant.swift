384a4fd0d97ce1e13e2c3977a61eddf493e8959f
atrejo79 <alvarojtrejo97@gmail.com>
2023-01-20 11:33:58 -0300

    20/01

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_multi_window
+import screen_retriever
+import url_launcher_macos
+import window_manager
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterMultiWindowPlugin.register(with: registry.registrar(forPlugin: "FlutterMultiWindowPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
---
+++ linux/base_flutter_window.h
@@ -0,0 +1,84 @@
+//
+// Created by boyan on 2022/1/27.
+//
+
+#ifndef DESKTOP_MULTI_WINDOW_LINUX_BASE_FLUTTER_WINDOW_H_
+#define DESKTOP_MULTI_WINDOW_LINUX_BASE_FLUTTER_WINDOW_H_
+
+#include <cmath>
+#include <gtk/gtk.h>
+#include <string>
+
+#include "window_channel.h"
+
+class BaseFlutterWindow {
+public:
+  virtual ~BaseFlutterWindow() = default;
+
+  virtual WindowChannel *GetWindowChannel() = 0;
+
+  BaseFlutterWindow();
+
+  void Show();
+
+  void Hide();
+
+  void Focus();
+
+  void SetFullscreen(bool fullscreen);
+
+  void Close();
+
+  void SetTitle(const std::string &title);
+
+  void SetBounds(double_t x, double_t y, double_t width, double_t height);
+  FlValue* GetBounds();
+
+  void Center();
+
+  void StartDragging();
+
+  void Minimize();
+
+  bool IsMaximized();
+
+  void Maximize();
+
+  void Unmaximize();
+
+  void ShowTitlebar(bool show);
+
+  void findEventBox(GtkWidget *widget);
+
+  void StartResizing(FlValue *value);
+
+  void Destroy();
+
+  bool IsPreventClose();
+
+  void SetPreventClose(bool setPreventClose);
+
+  int64_t GetXID();
+
+  bool isDragging = false;
+  bool isResizing = false;
+  GtkWidget *event_box = nullptr;
+  GdkEventButton currentPressedEvent = GdkEventButton{};
+
+  bool maximized = false;
+  bool minimized = false;
+  bool fullscreen = false;
+
+  bool isPreventClose = false;
+protected:
+  virtual GtkWindow *GetWindow() = 0;
+private:
+};
+
+gboolean onWindowEventAfter(GtkWidget *text_view, GdkEvent *event,
+                            BaseFlutterWindow *self);
+
+gboolean onMousePressHook(GSignalInvocationHint *ihint, guint n_param_values,
+                          const GValue *param_values, gpointer data);
+
+#endif // DESKTOP_MULTI_WINDOW_LINUX_BASE_FLUTTER_WINDOW_H_
